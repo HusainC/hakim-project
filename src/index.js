51c2a746fceee3b8f0694dd0a1c50fd5386133e1
Husain <hchopdawala@gmail.com>
2024-03-04 17:28:44 +0000

    Updated with kite ticker

--- src/index.js
+++ src/index.js
@@ -1,42 +1,64 @@
 import {WebSocket} from "ws"
-import { KiteConnect} from "kiteconnect"
+import { KiteConnect, KiteTicker} from "kiteconnect"
 
-
+//https://kite.zerodha.com/?action=login&type=login&status=success&request_token=PeLj7VGhepx0RqHUBpeMeCBwVpkApRS9
+const api_key = "ln8z5p6yuhu9h506";
+const api_secret = "n7x7z0hjntihmfgxfgdg2lnl3ubou58w"
 var kc = new KiteConnect({
-  api_key: "ln8z5p6yuhu9h506",
+  api_key,
 });
 
-const accessToken = await kc.generateSession("weUoH0OHtXnbcs6WCJztJYcnDryzXR8m", "n7x7z0hjntihmfgxfgdg2lnl3ubou58w")
+const accessToken = await kc.generateSession("PeLj7VGhepx0RqHUBpeMeCBwVpkApRS9", api_secret)
 
 console.log("check", accessToken)
 
 
-let socket = new WebSocket(`wss://ws.kite.trade?api_key=ln8z5p6yuhu9h506&access_token=${accessToken.access_token}`);
+var ticker = new KiteTicker({
+  api_key,
+  access_token: accessToken.access_token
+});
 
-socket.onopen = function(e) {
-  console.log("[open] Connection established");
-  console.log("Sending to server");
-  const message1 = { a: "subscribe", v: [408065] };
-  socket.send(JSON.stringify(message1));
-  const message = { a: "mode", v: ["full", [408065]] };
-  socket.send(JSON.stringify(message));
-};
+ticker.connect();
+ticker.on("ticks", onTicks);
+ticker.on("connect", subscribe);
 
-socket.onmessage = function(event) {
-    console.log("test connection", JSON.stringify(event))
-  console.log(`[message] Data received from server: ${JSON.stringify(event.data)}`);
-};
+function onTicks(ticks) {
+  console.log("Ticks", ticks);
+}
 
-socket.onclose = function(event) {
-  if (event.wasClean) {
-    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-  } else {
-    // e.g. server process killed or network down
-    // event.code is usually 1006 in this case
-    console.log('[close] Connection died');
-  }
-};
+function subscribe() {
+  var items = [408065];
+  ticker.subscribe(items);
+  ticker.setMode(ticker.modeFull, items);
+}
 
-socket.onerror = function(error) {
-  console.log(`[error]`);
-};
+
+// let socket = new WebSocket(`wss://ws.kite.trade?api_key=ln8z5p6yuhu9h506&access_token=${accessToken.access_token}`);
+
+// socket.onopen = function(e) {
+//   console.log("[open] Connection established");
+//   console.log("Sending to server");
+//   const message1 = { a: "subscribe", v: [408065] };
+//   socket.send(JSON.stringify(message1));
+//   const message = { a: "mode", v: ["full", [408065]] };
+//   socket.send(JSON.stringify(message));
+// };
+
+// socket.onmessage = function(event) {
+//     console.log("test connection", JSON.stringify(event))
+//   console.log(`[message] Data received from server: ${JSON.stringify(event.data)}`);
+// };
+
+// socket.onclose = function(event) {
+//   if (event.wasClean) {
+//     console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+//   } else {
+//     // e.g. server process killed or network down
+//     // event.code is usually 1006 in this case
+//     console.log('[close] Connection died');
+//   }
+// };
+
+// socket.onerror = function(error) {
+//   console.log(`[error]`);
+// };
